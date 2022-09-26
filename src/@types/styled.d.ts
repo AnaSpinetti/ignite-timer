e6ebb267306970e41e5349044db1134c80ac3795
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-09-26 00:46:15 -0300

    Criada a estrutura da página home - pendente estilização

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -3,6 +3,7 @@ import {defaultTheme} from '../styles/themes/default';
 
 type ThemeType = typeof defaultTheme;
 
+//Sobrescrevendo uma tipagem para o styled components
 declare module 'styled-components' {
     export interface DefaultTheme extends ThemeType {}
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,16 @@
 import { HeaderContainer } from "./styles";
+import logoImg from '../../../public/images/logo.svg';
+import { Timer, Scroll } from "phosphor-react";
+import { NavLink } from "react-router-dom";
 
 export function Header(){
     return (
-        <HeaderContainer />
+        <HeaderContainer>
+            <img src={logoImg} alt="" />
+            <nav>
+                <NavLink to="/" title="Timer"><Timer /></NavLink>
+                <NavLink to="/history" title="Histórico"><Scroll /></NavLink>
+            </nav>
+        </HeaderContainer>
     )
 }
