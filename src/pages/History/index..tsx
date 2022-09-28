bd4dd601656c95f20f89f2f87154ca74375e0519
AnaSpinetti <30602516+AnaSpinetti@users.noreply.github.com>
2022-09-28 01:04:23 -0300

    Interface finalizada

--- src/pages/History/index..tsx
+++ src/pages/History/index..tsx
@@ -1,3 +1,31 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
 export function History(){
-    return <p>'teste'</p>
+    return (
+        <HistoryContainer>
+            <h1>Meu histórico</h1>
+
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <Status statusColor="yellow">Status</Status>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>Tarefa</td>
+                            <td>20 Minutos</td>
+                            <td>Há 2 meses</td>
+                            <td>Concluído</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </HistoryContainer>
+    )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,40 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import { useForm } from "react-hook-form";
+import { CountdownContainer, FormContainer, HomeContainer, Separator, StartCountdownButton, TaskInput, TaskInputAmount } from "./styles";
+
+interface NewCycleFormData{
+    task: string,
+    minutesAmount: number
+}
 
 export function Home() {
+    const {register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
+        defaultValues: {
+            task: '',
+            minutesAmount: 0
+        }
+    });
+
+    // Verificando se tem valor no input de task para habilitar o botão de submit
+    const task = watch('task')
+
+    function handleSubmitForm(data: NewCycleFormData){
+        reset();
+    }
+    
     return (
         <HomeContainer>
-            <form>
+            <form onSubmit={handleSubmit(handleSubmitForm)}>
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <input id="task" />
+                    <TaskInput id="task" placeholder="Dê um nome para o seu projeto" {...register('task')} />
 
                     <label htmlFor="minutesAmount">durante</label>
-                    <input type="number" id="minutesAmount" />
+                    <TaskInputAmount list="task-suggestions" placeholder="00" step={2} min={1} max={60} type="number" id="minutesAmount" {...register('minutesAmount', {valueAsNumber: true})} />
+                    
+                    <datalist id="task-suggestions">
+                        <option value="" />
+                    </datalist>
 
                     <span>minutos.</span>
                 </FormContainer>
@@ -23,10 +47,10 @@ export function Home() {
                     <span>0</span>
                 </CountdownContainer>
 
-                <button type="submit">
+                <StartCountdownButton disabled={!task} type="submit">
                     <Play size={24} />
-                     Começar
-                </button>
+                    Começar
+                </StartCountdownButton>
             </form>
         </HomeContainer>
     )
